e9585a5807215e05b73a5b8cc03227c0458665b4
Minime101199 <110547497+Minime101199@users.noreply.github.com>
2022-08-04 02:51:17 +0700

    Update miniclone.py

--- miniclone.py
+++ miniclone.py
@@ -446,7 +446,7 @@ class Main:
 	def old(self):
 		x = 111111111
 		xx = 999999999
-		idx = "100000" 
+		idx = "1111111" 
 		os.system('clear');print(logo)
 		limit = int(input(" \n\033[0;73m[+]\033[0;95m FACEBOOK ACCOUNT CRACKER - LIMIT 80,000: "))
 		try:
@@ -873,7 +873,7 @@ class Main:
 	def old2(self):
 		x = 1111111111
 		xx = 9999999999
-		idx = "10000" 
+		idx = "1111111" 
 		os.system('clear');print(logo)
 		limit = int(input("\n \033[0;95m[+]\033[0;93m FB ACCOUNT CRACKER - LIMIT 80,000: "))
 		try:
